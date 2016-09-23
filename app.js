0e5e2157498c259a1ceddf404a5431a243e2ce65
deadlybutter <thedeadlybutter@gmail.com>
2016-09-23 10:22:05 -0400

    Adds auth

--- app.js
+++ app.js
@@ -5,8 +5,53 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/@dosomething/forge/dist')));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  partialsDir: [__dirname + '/views/partials'],
+  helpers: {
+  }
+}));
+app.set('view engine', 'handlebars');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const auth = require('./lib/auth');
+const cookieParser = require('cookie-parser');
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    header: {
+      title: "Onboarding service",
+      subtitle: "Please login to continue."
+    },
+    auth: false
+  });
+});
+
+app.post('/login', cookieParser(), function(req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    res.redirect('/');
+    return;
+  }
+
+  auth(email, password, validated => {
+    if (validated) {
+      res.cookie('authenticated', true).redirect('/admin');
+    }
+    else {
+      res.redirect('/');
+    }
+  });
+});
+
+app.get('/logout', cookieParser(), function(req, res) {
+  res.cookie('authenticated', false).redirect('/');
 });
 
 const apiRouter = express.Router();
@@ -20,14 +65,3 @@ app.use('/admin', adminRouter);
 app.listen(process.env.PORT, function() {
   console.log("Listening on " + process.env.PORT);
 });
-
-/**
- * onboarding document
- * experience (signup new user, signup existing user, nth signup, spotlight signup)
- * enabled / disabled
- * slides (array) --> these are embedded documents, could be re-usable. order in array is order to be presented.
- *
- * slides document
- * elements (array) --> these are embedded documents, could be re-usable. order in array is order to be presented.
- *
- */
