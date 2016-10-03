ee093aed9eed1812f614be68f2cb6edba6b19c67
deadlybutter <thedeadlybutter@gmail.com>
2016-10-03 10:19:07 -0400

    Fixing up the dev build scripts and adding files to the template

--- bin/build_dev.sh
+++ bin/build_dev.sh
@@ -1,1 +1,3 @@
-node-sass --watch ./public/scss ./public/scss/main.scss -o ./public/build/ | ./node_modules/.bin/babel ./public/js/main.js --watch --source-maps --out-file ./public/build/main.js | ./node_modules/.bin/babel ./public/js/components --watch --source-maps --out-file ./public/build/components.js
+node-sass --output-style compressed ./scss/main.scss --watch ./scss -o ./public/css/
+./node_modules/.bin/babel ./js/main.js --watch --source-maps --out-file ./public/js/main.js
+./node_modules/.bin/babel ./js/components --watch --source-maps --out-file ./public/js/components.js
