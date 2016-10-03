3cbfb135bbc14f52782123333dd15710b96a9146
deadlybutter <thedeadlybutter@gmail.com>
2016-10-03 11:10:31 -0400

    Fixing setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,7 @@
 mkdir public
 mkdir public/js
 npm run build
-if [ -n "PRODUCTION_ASSETS" ]
+if [[ -z "${PRODUCTION_ASSETS}" ]];
 then
   {
     # npm run build watches for changes, so this kills it after the assets has def. finished building
